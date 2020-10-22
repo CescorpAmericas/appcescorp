00b4405a9295b3431423e23772193c8974ebb3e3
Jesus Cenobio <sistemas@cescorp-americas.com>
2020-10-22 16:04:31 -0500

    Se realizo otra modificacion

--- resources/views/mod_administracion/repviajes/show.blade.php
+++ resources/views/mod_administracion/repviajes/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
       <div class="app-title">
         <div>
-          <h1><i class="fa fa-truck"></i> Reporte de viaje </h1>
+          <h1><i class="fa fa-truck"></i> Reporte de viaje 1 </h1>
         </div>
         <ul class="app-breadcrumb breadcrumb">
           <li class="breadcrumb-item"><a href="{{ route('repviajes.index')}}"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a></li>
